ddae1a7af693f54affb5cd88d27921ef5d800be1
Xevi <xbarnada@gmail.com>
2018-06-01 19:28:32 +0200

    Remove hstore install extension

--- bard/schema.sql
+++ bard/schema.sql
@@ -1,4 +1,3 @@
-CREATE EXTENSION HSTORE;
 CREATE TABLE cache_way (id BIGINT, version INTEGER, tag hstore);
 SELECT AddGeometryColumn ('public','cache_way','geom',4326,'LINESTRING',2, false);
 CREATE INDEX ON cache_node(id);
