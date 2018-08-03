06fd3e4c08513b73dc7956ee4b1df63bb45bf48f
Xevi <xbarnada@gmail.com>
2018-08-03 17:43:34 +0200

    Remove schema.sql

--- bard/schema.sql
+++
@@ -1,7 +0,0 @@
---CREATE EXTENSION POSTGIS;
---SELECT AddGeometryColumn ('public','cache_node','geom',4326,'POINT',2, false);
---CREATE TABLE cache_way (id BIGINT, version INTEGER);
---CREATE TABLE cache_way (id BIGINT, version INTEGER, tag hstore);
---SELECT AddGeometryColumn ('public','cache_way','geom',4326,'LINESTRING',2, false);
-CREATE INDEX ON cache_node(id);
-CREATE INDEX ON cache_node(version);
